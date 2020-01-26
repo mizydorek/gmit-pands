63041b3c4a7aafaa5355db62a28d5a3eabb4a5c5
Izydorek <maciej.izydorek@emc.com>
2020-01-26 12:16:54 +0000

    latest update

--- bmi.py
+++ bmi.py
@@ -1,5 +1,5 @@
-weight = int(input('Enter weight in kg: '))
-height = int(input('Enter height in cm: '))
+weight = float(input('Enter weight in kg: '))
+height = float(input('Enter height in cm: '))
 
 bmi = round(weight / (height/100) ** 2,2)
 print('BMI is', bmi)
